489f1f3285b9819c1f6c9363ab458482198a0cf5
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-03-11 19:07:22 +0530

    Update 29.c

--- 29.c
+++ 29.c
@@ -6,13 +6,18 @@ int main()
     char a[10000];
     printf("Input :\n");
     scanf("%d%d",&x,&y);
-    printf("Output :\n");
+    printf("Output    :\n");
     
     for(i=x;i<=y;i++)
     {
         if(i*i<=y)
         k++;
     }
-       printf("%d",k);   
+    
+    
+    printf("%d",k);   
     return 0;
 }
+
+
+
