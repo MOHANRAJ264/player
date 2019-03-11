be77815096a6b2887be7694eda3c1699a1e85079
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-03-11 18:44:17 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k,l,m,n;
+    char a[1000],b[1000];
+    printf("Input :\n"):
+    scanf("%d",&m);
+    printf("output :\n");
+     for(i=2;i<m;i++)
+     {
+         if(m%i==0)
+         {
+             n=1;
+             
+             
+         }
+     }
+     if(n==1)
+     printf("yes");
+     else
+     printf("no");
+    return 0;
+    
+}
