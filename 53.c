7e1f19bd67eb18f9a5aa41c17f16c7cd593fc6e2
MOHAN RAJ.T <46838625+MOHANRAJ264@users.noreply.github.com>
2019-04-01 18:16:22 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int i,j,k,l,m,n,p,a,b[10000];
+    char c[10000];
+    printf("Input :\n");
+    
+    gets(c);
+    printf("Output :\n");
+    
+    for(i=0;c[i]!='\0';i++);
+    
+    
+    
+   
+    printf("%d ",i);
+    
+    
+}
